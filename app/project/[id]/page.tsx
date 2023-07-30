6641038352af5721e908021e716eacd893091813
Abdullah <abdulluhodulate@gmail.com>
2023-07-30 15:20:46 +0200

    update image path

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import Navbar from "@/app/components/Navbar";
-import project01 from "../../../public/img01.jpg";
+import project01 from "../../../public/images/img01.jpg";
+// import project01 from "../../../public/img01.jpg";
 import allProps from "@/app/components/data/Properties.json";
 import { RiRadioButtonFill } from "react-icons/ri";
 
