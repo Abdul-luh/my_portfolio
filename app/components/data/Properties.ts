5a041e2d165729464dbba9f7f7cfc49c3b642913
Abdullah <abdulluhodulate@gmail.com>
2023-08-01 09:17:21 +0200

    added a new project

--- app/components/data/Properties.ts
+++ app/components/data/Properties.ts
@@ -3,10 +3,18 @@ import segam from "../../../public/images/segam.png";
 import acme from "../../../public/images/acme.png";
 import newsHompage from "../../../public/images/news-homepage.png";
 import calculator from "../../../public/images/calculator.png";
+import todoList from "../../../public/images/todo-list.png";
+
+let lastAssignedId = 0;
+
+function getNextId() {
+	lastAssignedId += 1;
+	return lastAssignedId;
+}
 
 const allProps = [
 	{
-		id: 1,
+		id: 0,
 		projName: "ACME",
 		projHead: "react | tailwind ",
 		projImg: acme,
@@ -20,7 +28,7 @@ const allProps = [
 		repoLink: "https://github.com/Abdul-luh/Tailwind-project/",
 	},
 	{
-		id: 2,
+		id: 0,
 		projName: "Segam Mobile Service",
 		projHead: "HTML | CSS | TAILWIND |JS ",
 		projImg: segam,
@@ -36,8 +44,8 @@ const allProps = [
 		repoLink: "https://github.com/Abdul-luh/segam-mobile-service",
 	},
 	{
-		id: 3,
-		projName: "Simple Responsive Website",
+		id: 0,
+		projName: "PortFolio sites",
 		projHead: "html | css",
 		projImg: projImg01,
 		projDescr:
@@ -51,7 +59,7 @@ const allProps = [
 		repoLink: "https://github.com/Abdul-luh/Simple_resposive_website",
 	},
 	{
-		id: 4,
+		id: 0,
 		projName: "News Hompage main",
 		projHead: "html | css ",
 		projImg: newsHompage,
@@ -68,37 +76,22 @@ const allProps = [
 		repoLink: "https://github.com/Abdul-luh/news-homepage-main",
 	},
 	{
-		id: 5,
-		projName: "simple webpage",
-		projHead: "react | tailwind",
+		id: 0,
+		projName: "Note-Pad App",
+		projHead: "html ",
 		projImg: projImg01,
 		projDescr:
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dolor a optio inventore. Facere eos ipsum voluptates fuga repudiandae eligendi labore! Cum, reiciendis, quidem excepturi totam magnam aut soluta fuga ullam iusto doloremque commodi, qui perferendis inventore quis beatae sapiente iste ut ad vel consequuntur veniam modi. Facere, porro iusto!",
 		projTech: [
-			{ id: 1, techName: "Node" },
-			{ id: 2, techName: "TAILWIND" },
+			{ id: 1, techName: "html" },
+			{ id: 2, techName: "css" },
 			{ id: 3, techName: "TYPESCRIPT" },
 		],
-		demoLink: "/",
-		repoLink: "/",
+		demoLink: "https://abdul-luh.github.io/NoteApp-Project/",
+		repoLink: "https://github.com/Abdul-luh/NoteApp-Project",
 	},
 	{
-		id: 6,
-		projName: "simple webpage",
-		projHead: "react | tailwind",
-		projImg: projImg01,
-		projDescr:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dolor a optio inventore. Facere eos ipsum voluptates fuga repudiandae eligendi labore! Cum, reiciendis, quidem excepturi totam magnam aut soluta fuga ullam iusto doloremque commodi, qui perferendis inventore quis beatae sapiente iste ut ad vel consequuntur veniam modi. Facere, porro iusto!",
-		projTech: [
-			{ id: 1, techName: "Node" },
-			{ id: 2, techName: "TAILWIND" },
-			{ id: 3, techName: "TYPESCRIPT" },
-		],
-		demoLink: "/",
-		repoLink: "/",
-	},
-	{
-		id: 7,
+		id: 0,
 		projName: "calculator",
 		projHead: "html | css | js",
 		projImg: calculator,
@@ -113,35 +106,39 @@ const allProps = [
 		repoLink: "https://github.com/Abdul-luh/Calculator-app",
 	},
 	{
-		id: 8,
-		projName: "simple webpage",
-		projHead: "react | tailwind",
-		projImg: projImg01,
+		id: 0,
+		projName: "Alarm Clock",
+		projHead: "html | css | js",
+		projImg: calculator,
 		projDescr:
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dolor a optio inventore. Facere eos ipsum voluptates fuga repudiandae eligendi labore! Cum, reiciendis, quidem excepturi totam magnam aut soluta fuga ullam iusto doloremque commodi, qui perferendis inventore quis beatae sapiente iste ut ad vel consequuntur veniam modi. Facere, porro iusto!",
 		projTech: [
-			{ id: 1, techName: "Node" },
+			{ id: 1, techName: "html" },
 			{ id: 2, techName: "TAILWIND" },
 			{ id: 3, techName: "TYPESCRIPT" },
 		],
-		demoLink: "/",
-		repoLink: "/",
+		demoLink: "https://abdul-luh.github.io/AlarmClock/",
+		repoLink: "https://github.com/Abdul-luh/AlarmClock",
 	},
 	{
-		id: 9,
-		projName: "simple webpage",
-		projHead: "react | tailwind",
-		projImg: projImg01,
+		id: 0,
+		projName: " todo List",
+		projHead: "html | css | js",
+		projImg: calculator,
 		projDescr:
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro dolor a optio inventore. Facere eos ipsum voluptates fuga repudiandae eligendi labore! Cum, reiciendis, quidem excepturi totam magnam aut soluta fuga ullam iusto doloremque commodi, qui perferendis inventore quis beatae sapiente iste ut ad vel consequuntur veniam modi. Facere, porro iusto!",
 		projTech: [
-			{ id: 1, techName: "Node" },
+			{ id: 1, techName: "html" },
 			{ id: 2, techName: "TAILWIND" },
 			{ id: 3, techName: "TYPESCRIPT" },
 		],
-		demoLink: "/",
-		repoLink: "/",
+		demoLink: "https://abdul-luh.github.io/todo-list-app_project/",
+		repoLink: "https://github.com/Abdul-luh/todo-list-app_project",
 	},
 ];
+
+allProps.forEach((item) => {
+	item.id = getNextId();
+});
 
 export default allProps;
