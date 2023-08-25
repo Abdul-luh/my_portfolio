dd558517ba55a7e9075d85be0676a7b7c952a046
Abdullah <abdulluhodulate@gmail.com>
2023-08-25 20:59:56 +0200

    created login and home route and databas modes +

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import skills from "./data/skill";
 import { StaticImageData } from "next/image";
+import SkillsCard from "./SkillsCard";
 
 interface MySkills {
 	id: number;
@@ -20,25 +20,11 @@ const Skills = () => {
 				<div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
 					{skills.map((skill: MySkills) => {
 						return (
-							<div
+							<SkillsCard
 								key={skill.id}
-								className="p-6 shadow-xl hover:scale-105 ease-in duration-300 rounded-xl">
-								<div className="grid grid-cols-2 gap-4 justify-center items-center">
-									<div className="m-auto">
-										<Image
-											src={skill.img}
-											width="60"
-											height="60"
-											alt={skill.skill}
-										/>
-									</div>
-									<div
-										className="flex flex-col items-center justify-center
-							">
-										<h3 className="uppercase">{skill.skill}</h3>
-									</div>
-								</div>
-							</div>
+								imgProp={skill.img}
+								text={skill.skill}
+							/>
 						);
 					})}
 				</div>
