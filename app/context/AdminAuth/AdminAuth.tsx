9dded9b1c4ede44d195d01f46238fbd71d21c83f
Abdullah <abdulluhodulate@gmail.com>
2024-05-08 04:30:42 +0100

    included a null option adminAuth.tsx

--- app/context/AdminAuth/AdminAuth.tsx
+++ app/context/AdminAuth/AdminAuth.tsx
@@ -17,6 +17,8 @@ interface AdminAuthContext_Iface {
 	isAuth: boolean;
 }
 
-export const AdminAuthContext = createContext<AdminAuthContext_Iface>(null);
+export const AdminAuthContext = createContext<AdminAuthContext_Iface | null>(
+	null
+);
 
 export default AdminAuthContext;
