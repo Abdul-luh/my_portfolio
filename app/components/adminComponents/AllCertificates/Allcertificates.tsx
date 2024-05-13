2e480b07413482e9d31c4684aa0fb82072070014
Abdullah <abdulluhodulate@gmail.com>
2024-05-13 23:02:18 +0100

    added handleDelete to allCertificate

--- app/components/adminComponents/AllCertificates/Allcertificates.tsx
+++ app/components/adminComponents/AllCertificates/Allcertificates.tsx
@@ -1,13 +1,25 @@
 import certificates from "../../data/Certificates";
 import { StaticImageData } from "next/image";
 import Card from "../Card";
+import axios from "axios";
 
 interface Cert {
 	url: StaticImageData;
 	name: string;
 }
 
-const AllTechnologies = () => {
+const handleDelete = async (id: string) => {
+	try {
+		const res = await axios.delete(`/api/technologies/${id}`);
+		// const updatteTechnologies =
+		const data = await res.data;
+		console.log(data);
+	} catch (error: any) {
+		console.error(error);
+	}
+};
+
+const AllCertificates = () => {
 	return (
 		<section id="AllCertificates" className={"w-full  p-4"}>
 			{/* <p className="text-xl tracking-widest uppercase text-[#5651e5]">
@@ -16,7 +28,14 @@ const AllTechnologies = () => {
 			<div className="max-w-[1240px] mx-auto h-full flex flex-col justify-center items-center">
 				<div className="grid  gap-8">
 					{certificates.map((cert: Cert) => {
-						return <Card key={cert.name} text={cert.name} />;
+						return (
+							<Card
+								key={cert.name}
+								id={cert.name}
+								handleDelete={handleDelete}
+								text={cert.name}
+							/>
+						);
 					})}
 				</div>
 			</div>
@@ -24,4 +43,4 @@ const AllTechnologies = () => {
 	);
 };
 
-export default AllTechnologies;
+export default AllCertificates;
